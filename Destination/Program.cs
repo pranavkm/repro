4c9bfa3b555ac8afe0395c29ddf05bcd0d38ee21
Pranav K <prkrishn@hotmail.com>
2018-09-26 11:40:21 -0700

    Repro for https://github.com/aspnet/Home/issues/2780

---
+++ Destination/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Destination
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseUrls("http://localhost:12000")
+                .ConfigureLogging(l => l.AddConsole().SetMinimumLevel(LogLevel.Trace))
+                .UseStartup<Startup>();
+    }
+}
