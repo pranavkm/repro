79e79dc2686a1242847581c16ba15de5652306a3
Pranav K <prkrishn@hotmail.com>
2019-06-10 16:50:51 -0700

    WIP6

--- Program.cs
+++ Program.cs
@@ -22,7 +22,7 @@ namespace repro
             var culture = new CultureInfo("fr-FR");
             Console.WriteLine(culture.DisplayName);
 
-            var @decimal = "32 000,1";
+            var @decimal = "32000,1";
             try
             {
                 var d = Decimal.Parse(@decimal, NumberStyles.Float | NumberStyles.AllowThousands, culture);
@@ -33,126 +33,6 @@ namespace repro
                 Console.WriteLine(e);
             }
 
-        }
-
-        private static void Test()
-        {
-            LoggerFactory = new LoggerFactory()
-                            .AddConsole(LogLevel.Trace);
-
-
-            var bindingContext = MainAsync().GetAwaiter().GetResult();
-
-            LoggerFactory.Dispose();
-
-            System.Console.WriteLine(bindingContext.Result.Model);
-            Console.WriteLine("IsModelSet: " + bindingContext.Result.IsModelSet);
-            // Console.WriteLine(32_000.1M == (decimal)bindingContext.Result.Model);
-            Console.WriteLine("Contains: " + bindingContext.ModelState.ContainsKey("theModelName"));
-
-            var modelState = bindingContext.ModelState["theModelName"];
-            if (!modelState.Errors.Any())
-            {
-                Console.WriteLine("No errors");
-            }
-            else
-            {
-                Console.WriteLine("has errors");
-                foreach (var error in modelState.Errors)
-                {
-                    Console.WriteLine(error.ErrorMessage + " " + error.Exception);
-
-                }
-            }
-        }
-
-        public static async Task<DefaultModelBindingContext> MainAsync()
-        {
-            // Arrange
-            var bindingContext = GetBindingContext(typeof(decimal));
-            bindingContext.ValueProvider = new SimpleValueProvider(new CultureInfo("fr-FR"))
-            {
-                { "theModelName", "32 000,1" }
-            };
-            var binder = GetBinder();
-
-            // Act
-            await binder.BindModelAsync(bindingContext);
-
-            // Assert
-            return bindingContext;
-        }
-
-        private static DefaultModelBindingContext GetBindingContext(Type modelType)
-        {
-            return new DefaultModelBindingContext
-            {
-                ModelMetadata = new EmptyModelMetadataProvider().GetMetadataForType(modelType),
-                ModelName = "theModelName",
-                ModelState = new ModelStateDictionary(),
-                ValueProvider = new SimpleValueProvider() // empty
-            };
-        }
-
-        private static IModelBinder GetBinder()
-        {
-            return new DecimalModelBinder(NumberStyles.Float | NumberStyles.AllowThousands, LoggerFactory);
-        }
-    }
-
-
-    public sealed class SimpleValueProvider : Dictionary<string, object>, IValueProvider
-    {
-        private readonly CultureInfo _culture;
-
-        public SimpleValueProvider()
-            : this(null)
-        {
-        }
-
-        public SimpleValueProvider(CultureInfo culture)
-            : base(StringComparer.OrdinalIgnoreCase)
-        {
-            _culture = culture ?? CultureInfo.InvariantCulture;
-        }
-
-        public bool ContainsPrefix(string prefix)
-        {
-            foreach (string key in Keys)
-            {
-                if (ModelStateDictionary.StartsWithPrefix(prefix, key))
-                {
-                    return true;
-                }
-            }
-
-            return false;
-        }
-
-        public ValueProviderResult GetValue(string key)
-        {
-            if (TryGetValue(key, out var rawValue))
-            {
-                if (rawValue != null && rawValue.GetType().IsArray)
-                {
-                    var array = (Array)rawValue;
-
-                    var stringValues = new string[array.Length];
-                    for (var i = 0; i < array.Length; i++)
-                    {
-                        stringValues[i] = array.GetValue(i) as string ?? Convert.ToString(array.GetValue(i), _culture);
-                    }
-
-                    return new ValueProviderResult(stringValues, _culture);
-                }
-                else
-                {
-                    var stringValue = rawValue as string ?? Convert.ToString(rawValue, _culture) ?? string.Empty;
-                    return new ValueProviderResult(stringValue, _culture);
-                }
-            }
-
-            return ValueProviderResult.None;
         }
     }
 }
