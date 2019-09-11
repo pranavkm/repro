de33d3ae999760032c984bdc6db355612e7ade3f
Pranav K <prkrishn@hotmail.com>
2019-09-11 16:34:12 -0700

    Transitive framework references?

---
+++ class1/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace class1
+{
+    public class Class1
+    {
+        public void Test()
+            => Console.WriteLine(typeof(Microsoft.AspNetCore.Mvc.IActionResult));
+    }
+}
---
+++ class2/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace class2
+{
+    public class Class1
+    {
+    }
+}
