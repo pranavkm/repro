d0ec6706125d22009c3ed5be0aab28995283c363
Pranav K <prkrishn@hotmail.com>
2020-09-21 16:35:47 -0700

    Repro

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.Versioning;
+
+namespace foo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Debug.Assert(OperatingSystem.IsWindows());
+            Unsupported();
+        }
+
+        [UnsupportedOSPlatform("windows")]
+        static void Unsupported()
+        {
+
+        }
+    }
+}
